771703dff81e8c6b8907b1a165f3ee24473c4d73
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-22 10:39:08 -0600

    7

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h1>Borah High School Wrestling</h1>
-<div>
+<div class="Nav_Bar">
 <ul>
 	<li><a href ="https://www.google.com/">Home</a></li>
 	<li><a href ="https://www.sierratradingpost.com/">Schedule</a></li>
