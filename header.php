ad784b55f9844e076f2c0c9c9ed69a11f6477574
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-22 09:37:39 -0600

    test

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Borah High School Wrestling</title>
+	<link rel="stylesheet" href="Style.css">
+<h1>Borah High School Wrestling</h1>
+<div>
+<ul>
+	<li><a href ="https://www.google.com/">Home</a></li>
+	<li><a href ="https://www.sierratradingpost.com/">Schedule</a></li>
+	<li><a href ="https://www.sportsmanswarehouse.com/">Wrestler Login</a></li>
+	<li><a href ="https://www.cabelas.com/">Fundraising</a></li>
+	<li><a href ="https://www.basspro.com/">Spirit Gear</a></li>
+	<li><a href ="http://vortexoptics.com/">Photos</a></li>
+	<li><a href ="https://www.nikon.com/">Coaching Staff</a></li>
+</ul>
+</div>
+
+</html>
