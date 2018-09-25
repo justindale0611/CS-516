75af5db73fb2d1651015bb07b492eca97440b802
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-25 11:23:04 -0600

    1

--- Schedule.php
+++ Schedule.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 
 <body  id = schedule>
-<p><object height="25%" width="25%" class="center" data="Schedule.pdf"></object>
+<p><object height="400" width="400" class="center" data="Schedule.pdf"></object>
 </p>
 </body>
 </html>
