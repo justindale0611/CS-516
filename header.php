7c63fca784f13a9d6709f66b463e4b6b47a8e6bc
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-24 20:49:04 -0600

    4

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
 <h1>
 	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
 </h1>
-<div>
+<div >
 <ul>
 	<li><a href ="Home.PHP">Home</a></li>
-	<li><a href ="https://www.sierratradingpost.com/">Schedule</a></li>
+	<li><a href ="Schedule.php">Schedule</a></li>
 	<li><a href ="https://www.sportsmanswarehouse.com/">Wrestler Login</a></li>
 	<li><a href ="https://www.cabelas.com/">Fundraising</a></li>
 	<li><a href ="https://www.basspro.com/">Spirit Gear</a></li>
