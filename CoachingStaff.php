6f4b07c4c2a2637b3fba50dccd8e5aa555365d6d
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-26 20:02:45 -0600

    1

--- CoachingStaff.php
+++ CoachingStaff.php
@@ -4,8 +4,9 @@
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 
 <body>
-<img src="CoachG.jpg" alt="Borah Wrestler" height="25%" width="25%" class="center">
+<p><img src="CoachG.jpg" alt="Borah Wrestler" height="25%" width="25%" class="center">
 <img src="CoachJ.jpg" alt="Borah Wrestler" height="25%" width="25%" class="center">
+</p>
 </body>
 <?php 
 	require_once("Footer.php");
