382e053a6d9117eebc08b3f3cf132c4f3f31dd08
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-09-26 18:40:10 -0600

    1

--- Footer.php
+++ Footer.php
@@ -1,1 +1,1 @@
-<div>Thank you for visiting our page. We appreciate your support. If you have any questions please contact Coach Gardner. Email: justin.gardner@boiseschools.org</div>
+<div class="footer">Thank you for visiting our page. We appreciate your support. If you have any questions please contact Coach Gardner. Email: justin.gardner@boiseschools.org</div>
