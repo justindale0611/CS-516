d101ceef4cd01a21310686a253f2732d7fec1cb9
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-10-03 20:11:25 -0600

    1

---
+++ Dao.php
@@ -0,0 +1,12 @@
+<?php
+class Dao {
+private $host = "us-cdbr-iron-east-01.cleardb.net";
+private $db = "heroku_b0950797e204108";
+private $user = "b0d08fe860ed9e";
+private $pass = "e9495ff9";
+public function getConnection () {
+return
+new PDO("mysql:host={$this->host};dbname={$this->db}"
+}
+
+}
