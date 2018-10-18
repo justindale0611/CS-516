839042083b864cca6ff6a283a882419454a1b2f7
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-10-17 19:21:06 -0600

    1

--- WrestlerLogin.php
+++ WrestlerLogin.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
-<?php require_once("header.php");?>
+<?php require_once("header.php");
+session_start();?>
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 
 <body>
 	<br>
-	<form method="post" action="login_handler.php">
+	<form method="POST" action="login_handler.php">
 			Login:<br>
 			<input type="text" name="login" value=""><br>
 			password:<br>
 			<input type="password" name="password">
+			<div><input type="submit" value="Login">    </div>
+
 		</form>
-		<button type="submit"> Login </button>
 	</body>
 <?php 
 	require_once("Footer.php");
