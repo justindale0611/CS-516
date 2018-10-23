0a64ddbb39cbe397cf1e73e78b62dddad3ac329f
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-10-22 20:19:36 -0600

    form fixes

--- WrestlerLogin.php
+++ WrestlerLogin.php
@@ -9,15 +9,16 @@ session_start();
 	<br>
 	<form method="POST" action="login_handler.php">
 			<label for="username">Username:</label><br>
+			<input type="text" name="username" required><br>
 			<?php if(isset($_SESSION['errors']['username'])) { ?>
 			<span id="usernameError" class="error"><?=$_SESSION['messages']['username']?></span>
 			<?php } ?>
-			<input type="text" name="username" value=""><br>
+
 			<label for="password">Password:</label><br>
+			<input type="password" name="password" required><br>
 			<?php if(isset($_SESSION['errors']['password'])) { ?>
 			<span id="passwordError" class="error"><?=$_SESSION['messages']['password']?></span>
 			<?php } ?>
-			<input type="password" name="password">
 			<button type="submit" value="Login">Login</button>
 		</form>
 	</body>
--- login_handler.php
+++ login_handler.php
@@ -15,14 +15,19 @@
     $_SESSION['errors']['password'] = "Password is required.";
     $bad = true;
   }
-  if ($bad) {
-    header('Location: WrestlerLogin.php');
-    exit;
-  }
+  // if ($bad) {
+  //   header('Location: WrestlerLogin.php');
+  //   exit;
+  // }
   // Got here, means everything validated, and the comment will post.
-  unset($_SESSION['presets']);
-  require_once 'Dao.php';
-  $dao->saveLogin($name, $password);
-  header('Location: Home.php');
-  exit;
+  if (empty($errors)) {
+    unset($_SESSION['presets']);
+    require_once 'Dao.php';
+    $dao->saveLogin($name, $password);
+    header('Location: Home.php');
+  } else {
+    header('WrestlerLogin.php');
+    die;
+  }
+  }
 ?>
