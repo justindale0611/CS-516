3933203376956320876e5682973abbbb062dce8f
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-10-22 20:43:34 -0600

    form fixes

--- Dao.php
+++ Dao.php
@@ -1,28 +1,40 @@
 <?php
 class Dao {
-private $host = "us-cdbr-iron-east-01.cleardb.net";
-private $db = "heroku_b0950797e204108";
-private $user = "b0d08fe860ed9e";
-private $pass = "e9495ff9";
-public function getConnection () {
-return
-new PDO("mysql:host={$this->host};dbname={$this->db}")
-}
+	private $host = "us-cdbr-iron-east-01.cleardb.net";
+	private $db = "heroku_b0950797e204108";
+	private $user = "b0d08fe860ed9e";
+	private $pass = "e9495ff9";
 
-public function saveLogin($name, $password) {
-	$connection =$this->getConnection();
-	 $saveQuery =
-        "INSERT INTO users (name, password)
-        VALUES (:name, :comment)";
-    $q = $conn->prepare($saveQuery);
-    $q->bindParam(":name", $name);
-    $q->bindParam(":password", $password);
-    $q->execute();
-}
+	public function getConnection () {
+		return new PDO("mysql:host={$this->host};dbname={$this->db}")
+	}
 
-		
-   
-        
+	public function saveLogin($name, $password) {
+		$connection =$this->getConnection();
+		 $saveQuery =
+	        "INSERT INTO users (name, password)
+	        VALUES (:name, :comment)";
+	    $q = $conn->prepare($saveQuery);
+	    $q->bindParam(":name", $name);
+	    $q->bindParam(":password", $password);
+	    $q->execute();
+	}
 
-  }
+	public function validateUser($username, $password) {
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT id, username, password FROM users WHERE users = :username");
+
+		$stmt->bindParam(':username', $username);	
+		$stmt->execute();
+
+		$user = $stmt->fetch();
+		if ($user) {
+			$digest = $user['password'];
+			if (password_verify($password, $digest)) {
+				return array('username' => $user['username'], 'id' => $user['id']);
+			}
+		} 
+		return false;
+	}
 }
+?>
--- login_handler.php
+++ login_handler.php
@@ -1,13 +1,14 @@
 <?php
+require_once('Dao.php');
   session_start();
-  $name = $_POST['username'];
+  $username = $_POST['username'];
   $password = $_POST['password'];
-  $_SESSION['presets']['username'] = $name;
+  $_SESSION['presets']['username'] = $username;
   $_SESSION['presets']['password'] = $password;
 
   $errors = array();
   $bad = false;
-  if (empty($name)) {
+  if (empty($username)) {
     $_SESSION['errors']['username'] = "Name is required.";
     $bad = true;
   }
@@ -21,13 +22,19 @@
   // }
   // Got here, means everything validated, and the comment will post.
   if (empty($errors)) {
-    unset($_SESSION['presets']);
-    require_once 'Dao.php';
-    $dao->saveLogin($name, $password);
-    header('Location: Home.php');
+    try {
+      $dao = new Dao();
+      $user = $dao->validateUser($username, $password);
+      if ($user) {
+        header('Home.php');
+      } else {
+        $errors['message'] = "Invalid username or password";
+      }
+    } catch (Exception $e) {
+      $errors['message' = "Something went wrong. Please come back later."]
+    }
   } else {
     header('WrestlerLogin.php');
     die;
-  }
   }
 ?>
