9ff7edd6915d4aa192373c435723d39746a21d52
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-10-24 20:15:23 -0600

    debugging

--- login_handler.php
+++ login_handler.php
@@ -1,47 +1,45 @@
 <?php
 require_once("Dao.php");
 session_start();
-echo "hello 1";
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $_SESSION['presets']['username'] = $username;
+$username = $_POST['username'];
+$password = $_POST['password'];
+$_SESSION['presets']['username'] = $username;
 
-  $errors = array();
-  unset( $_SESSION['presets']['password']);
-  $bad = false;
-  if (empty($username)) {
-    $_SESSION['errors']['username'] = "Name is required.";
-    $bad = true;
-  }
-  if (empty($password)) {
-    $_SESSION['errors']['password'] = "Password is required.";
-    $bad = true;
-  }
+$errors = array();
+unset( $_SESSION['presets']['password']);
+$bad = false;
+if (empty($username)) {
+  $_SESSION['errors']['username'] = "Name is required.";
+  $bad = true;
+}
+if (empty($password)) {
+  $_SESSION['errors']['password'] = "Password is required.";
+  $bad = true;
+}
 
-  if ($bad) {
-    echo "hello 2";
+if ($bad) {
+  header("Location: WrestlerLogin.php");
+  exit;
+}
+// Got here, means everything validated, and the comment will post.
+
+////THIS NEEDS WORK
+if (empty($errors)) {
+  try {
+    $dao = new Dao();
+    // $user = $dao->validateUser($username, $password);
+    // if ($user) {
+    //   header("Location: Home.php");
+    // } else {
+    //   $errors['message'] = "Invalid username or password";
+    //   header("Location: WrestlerLogin.php");
+    // }
+  } catch (Exception $e) {
+    $errors['message' = "Something went wrong. Please come back later."]
     header("Location: WrestlerLogin.php");
-    exit;
   }
-  // Got here, means everything validated, and the comment will post.
-
-  ////THIS NEEDS WORK
-  if (empty($errors)) {
-    try {
-      $dao = new Dao();
-      $user = $dao->validateUser($username, $password);
-      if ($user) {
-        header("Location: Home.php");
-      } else {
-        $errors['message'] = "Invalid username or password";
-        header("Location: WrestlerLogin.php");
-      }
-    } catch (Exception $e) {
-      $errors['message' = "Something went wrong. Please come back later."]
-      header("Location: WrestlerLogin.php");
-    }
-  } else {
-    header("Location: WrestlerLogin.php");
-    die;
-  }
+} else {
+  header("Location: WrestlerLogin.php");
+  die;
+}
 ?>
