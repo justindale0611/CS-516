d67e2a5565a94be5d80d242a648f97c97237d6c2
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-07 07:37:24 -0700

    1

--- Home.php
+++ Home.php
@@ -1,8 +1,26 @@
 <!DOCTYPE html>
 <html>
-<?php 
-	include "header.php";
-?>
+<head>
+	<title>Borah High School Wrestling</title>
+	<link rel="stylesheet" href="Style.CSS">
+	<link rel="icon" href="Logo.jpg" type="image/gif" sizes="16x16">
+</head>
+<h1>
+	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
+</h1>
+<div id="navigation">
+<ul class="nav">
+	<li class="nav-item"><a href ="Home.php">Home</a></li>
+	<li class="nav-item"><a href ="Schedule.php">Schedule</a></li>
+	<!-- <li class="nav-item"><a href ="WrestlerLogin.php">Wrestler Login</a></li> -->
+<!-- 	<li class="nav-item"><a href ="CoachingStaff.php">Coaching Staff</a></li>
+ -->	
+ <li class="nav-item"><a href ="CoachingStaff.php">Login Out</a></li>
+	<li class="nav-item"><a href ="forms.php">Wrestler's Forms</a></li>
+
+</ul>
+</div>
+
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 <body>
 <p>Welcome to Borah High School's wrestling page. This page contains all information needed for wrestlers, parents, fans, and administrators. We cannot thank you enough for your support and look forward to seeing you at our next match.
