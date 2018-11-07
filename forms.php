29b55d7b8bdaa26fe413fdbb70e2ea979a827608
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-06 17:18:03 -0700

    1

---
+++ forms.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	include "header.php";
+?>
+<link rel="stylesheet" type="text/css" href="Style.CSS">
+<body>
+<p>THIS IS A BLANK PAGE LEFT HERE ON PURPOSE!!!! LEAVE ME ALONE PLEASE!!
+</p>
+</body>
+
+<?php 
+	require_once("Footer.php");
+?>
+</html>
+
