a32977c16faf6f7449f50eadb2510979c52b1091
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-06 17:05:38 -0700

    1

--- CoachingStaff.php
+++ CoachingStaff.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <html>
-<!-- <?php 
-	include "header.php";
-?> -->
+<head>
+	<title>Borah High School Wrestling</title>
+	<link rel="stylesheet" href="Style.CSS">
+	<link rel="icon" href="Logo.jpg" type="image/gif" sizes="16x16">
+</head>
+<h1>
+	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
+</h1>
+<div id="navigation">
+<ul class="nav">
+	<li class="nav-item"><a href ="index.php">Home</a></li>
+	<li class="nav-item"><a href ="Schedule.php">Schedule</a></li>
+	<li class="nav-item"><a href ="WrestlerLogin.php">Wrestler Login</a></li>
+	<li class="nav-item"><a href ="CoachingStaff.php">Coaching Staff</a></li>
+</ul>
+</div>
+
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 <body>
 <p>Welcome to Borah High School's wrestling page. Please Log in to begin. 
