17008246458a98cd1745652983596e008b0c9e3a
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-06 17:53:50 -0700

    1

--- forms.php
+++ forms.php
@@ -5,7 +5,7 @@
 ?>
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 <body>
-<p>THIS IS A BLANK PAGE LEFT HERE ON PURPOSE!!!! LEAVE ME ALONE PLEASE!!
+<p>Please fill out the following information. The information obtained here is for coaches only <br>
 <label for="wrestlersname">Wrestler's Name:</label><br>
 			<input type="text" name="wrestlersname" required><br>
 			<!-- <?php if(isset($_SESSION['errors']['username'])) { ?>
