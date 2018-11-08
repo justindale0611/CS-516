50b882a759137595ec5ee07d4c09c71d5b812760
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-07 22:12:23 -0700

    1

--- FormsHandler.php
+++ FormsHandler.php
@@ -3,28 +3,34 @@ require_once("Dao.php");
 session_start();
 
 
-
-
-$_SESSION['presets']['username'] = $username;
-
-unset($_SESSION['presets']['password']);
+$wname = $_POST['wrestlersname'];
+$wphone = $_POST['wrestlersphone'];
+$wemail = $_POST['wrestlersemail'];
+$pname = $_POST['parentsname'];
+$pphone = $_POST['parentsphone'];
+$pemail = $_POST['parentsemail'];
+echo $wname."    ";
+echo $wphone."    ";
+echo $wemail."    ";
+echo $pname."    ";
+echo $pphone."    ";
+echo $pemail."    ";
 
 $bad = false;
 if (empty($wname)) {
-    $_SESSION['formError'] = "Name is required.";
     $bad = true;
+
 }
 if (empty($wphone)) {
-    $_SESSION['formError'] = "Name is required.";
     $bad = true;
+    header("Location: forms.php");
+    exit;
 }
 
 if (empty($wemail)) {
-    $_SESSION['formError'] = "Name is required.";
     $bad = true;
 }
 if (empty($pname)) {
-    $_SESSION['formError'] = "Name is required.";
     $bad = true;
 }
 if (empty($pphone)) {
