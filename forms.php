350ae7ff177ad1981edc31b8477ce0234dd5419c
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-07 21:47:17 -0700

    1

--- forms.php
+++ forms.php
@@ -6,6 +6,8 @@
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 <body>
 <p>Please fill out the following information. The information obtained here is for coaches only <br>
+<form method="POST" action="FormsHandler.php">
+
 <label for="wrestlersname">Wrestler's Name:</label><br>
 			<input type="text" name="wrestlersname" required><br>
 			<!-- <?php if(isset($_SESSION['errors']['username'])) { ?>
@@ -25,14 +27,14 @@
 			<input type="text" name="parentsname" required><br>
 
 			<label for="parentsphone">Emergency Phone Number:</label><br>
-			<input type="tel" name="wrestlersphone" required><br>
+			<input type="tel" name="parentsphone" required><br>
 
 			<label for="paretsemail">Emergency Email:</label><br>
 			<input type="email" name="paretsemail" required><br>
 
 
 			<button type="submit" value="Submit" name="SubmitForms">Submit</button>
-			
+			</form>
 
 </p>
 </body>
