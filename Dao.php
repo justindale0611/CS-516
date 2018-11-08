407d023e4c7871c1f1e49c76966700d40f077c45
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-07 20:35:55 -0700

    1

--- Dao.php
+++ Dao.php
@@ -1,63 +1,68 @@
 <?php
-class Dao {
-	private $host = "us-cdbr-iron-east-01.cleardb.net";
-	private $dbname = "heroku_b0950797e204108";
-	private $user = "b0d08fe860ed9e";
-	private $password = "e9495ff9";
-
-	private function getConnection() {
-		$conn = new PDO ( "mysql:dbname={$this->dbname};host={$this->host};", "$this->user", "$this->password" );
-		$conn->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		return $conn;
-	}
-
-	public function userExists($username) {
-		$conn = $this->getConnection();
-		$q = $conn->prepare("SELECT * FROM users WHERE username = :username");
-		$q->bindParam(':username', $username);
-		$q->execute():
-		if ($q->fetch()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public function getConnectionStatus() {
-		$conn = $this->getConnection();
-		return $conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
-	}
-
-	public function createAccount($username, $password) {
-		$connection =$this->getConnection();
-		 $saveQuery =
-	        "INSERT INTO users (username, password)
-	        VALUES (:username, :password)";
-	    $q = $conn->prepare($saveQuery);
-	    $q->bindParam(":username", $username);
-	    $q->bindParam(":password", $password);
-	    $q->execute();
-	}
-
-	// public function getUsername($username){
-	// 	$conn=$this->getConnection();
-	// 	$q=$conn->prepare("SELECT username FROM users WHERE username='$username'");
-	// 	$q->bindParam(":username", $username);
-	// 	$q->setFetchMode(PDO::FETCH_ASSOC);
-	// 	$q->execute();
-	// 	$result=$q->fetchAll();
-	// 	return $result;
-	// }
-
-	public function validateUser($username, $password) {
-		$conn=$this->getConnection();
-		$q=$conn->prepare("select username from users where username='$username' and password='$password'");
-		$q->bindParam(":username", $username);
-		$q->bindParam(":password", $password);
-		$q->setFetchMode(PDO::FETCH_ASSOC);
-		$q->execute();
-		$result=$q->fetchAll();
-		return $result;
-	}
+class Dao
+{
+    private $host = "us-cdbr-iron-east-01.cleardb.net";
+    private $dbname = "heroku_b0950797e204108";
+    private $user = "b0d08fe860ed9e";
+    private $password = "e9495ff9";
+    
+    private function getConnection()
+    {
+        $conn = new PDO("mysql:dbname={$this->dbname};host={$this->host};", "$this->user", "$this->password");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+    
+    public function userExists($username)
+    {
+        $conn = $this->getConnection();
+        $q    = $conn->prepare("SELECT * FROM users WHERE username = :username");
+        $q->bindParam(':username', $username);
+        $q->execute();
+        if ($q->fetch()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function getConnectionStatus()
+    {
+        $conn = $this->getConnection();
+        return $conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
+    }
+    
+    public function createAccount($username, $password)
+    {
+        $connection = $this->getConnection();
+        $saveQuery  = "INSERT INTO users (username, password)
+            VALUES (:username, :password)";
+        $q          = $conn->prepare($saveQuery);
+        $q->bindParam(":username", $username);
+        $q->bindParam(":password", $password);
+        $q->execute();
+    }
+    
+    // public function getUsername($username){
+    //     $conn=$this->getConnection();
+    //     $q=$conn->prepare("SELECT username FROM users WHERE username='$username'");
+    //     $q->bindParam(":username", $username);
+    //     $q->setFetchMode(PDO::FETCH_ASSOC);
+    //     $q->execute();
+    //     $result=$q->fetchAll();
+    //     return $result;
+    // }
+    
+    public function validateUser($username, $password)
+    {
+        $conn = $this->getConnection();
+        $q    = $conn->prepare("select username from users where username='$username' and password='$password'");
+        $q->bindParam(":username", $username);
+        $q->bindParam(":password", $password);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $q->execute();
+        $result = $q->fetchAll();
+        return $result;
+    }
 }
 ?>
--- login_handler.php
+++ login_handler.php
@@ -1,62 +1,63 @@
 <?php
 require_once("Dao.php");
-//session_start();
+session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username                        = $_POST['username'];
+$password                        = $_POST['password'];
 $_SESSION['presets']['username'] = $username;
 
 $errors = array();
-unset( $_SESSION['presets']['password']);
+unset($_SESSION['presets']['password']);
 $bad = false;
 
 if (empty($username)) {
-  $_SESSION['errors']['username'] = "Name is required.";
-  $bad = true;
+    $_SESSION['errors']['username'] = "Name is required.";
+    $bad                            = true;
 }
 
 if (empty($password)) {
-  $_SESSION['errors']['password'] = "Password is required.";
-  $bad = true;
+    $_SESSION['errors']['password'] = "Password is required.";
+    $bad                            = true;
 }
-if (!preg_match('~[0-9]~', $password)||!preg_match('~[a-z]~', $password)) {
-    $_SESSION['message'][] = "Password must have at least one number and one lowercase letter."; 
-    $bad=true;
-  }
+if (!preg_match('~[0-9]~', $password) || !preg_match('~[a-z]~', $password)) {
+    $_SESSION['message'][] = "Password must have at least one number and one lowercase letter.";
+    $bad                   = true;
+}
 
 if ($bad) {
-  header("Location: WrestlerLogin.php");
-  exit;
+    header("Location: WrestlerLogin.php");
+    exit;
 }
 
 ////THIS NEEDS WORK
 if (empty($errors)) {
-  try {
-    $dao = new Dao();
-
-    //Login
-    if (isset ($_POST['LoginButton'])) {
-      $user = $dao->validateUser($username, $password);
-      if ($user) {
-          header('Location: Home.php');
-      } else {
-          header('Location: Schedule.php');
-      }
-
-    //Create Account
-    } else if (isset($_POST['CreateAccountButton'])) {
-      $exist = $dao->userExists($username);
-      if($exist) {
-        header('Location: Schedule.php');         
-      } else {
-        //$dao->createAccount($username, $password);
-        header('Location: Home.php');
-      }
+    try {
+        $dao = new Dao();
+        
+        //Login
+        if (isset($_POST['LoginButton'])) {
+            $user = $dao->validateUser($username, $password);
+            if ($user) {
+                header('Location: Home.php');
+            } else {
+                header('Location: Schedule.php');
+            }
+            
+            //Create Account
+        } else if (isset($_POST['CreateAccountButton'])) {
+            $exist = $dao->userExists($username);
+            if ($exist) {
+                header('Location: Schedule.php');
+            } else {
+                $dao->createAccount($username, $password);
+                header('Location: Home.php');
+            }
+        }
+        
     }
-
-  } catch (Exception $e) {
-    header('Location: Schedule.php');
-  }
+    catch (Exception $e) {
+        header('Location: Schedule.php');
+    }
 }
 
 ?>
