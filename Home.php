bdfe548a4b2961a58dd49f8592f7ae2599ba090b
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-09 15:28:09 -0700

    1

--- Home.php
+++ Home.php
@@ -15,8 +15,8 @@
 	<link rel="icon" href="Logo.jpg" type="image/gif" sizes="16x16">
 </head>
 <h1>
-	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
-</h1>
+<!-- 	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
+ --></h1>
 <div id="navigation">
 <ul class="nav">
 	<li class="nav-item"><a href ="Home.php">Home</a></li>
