059eedb97390c7f2b24d8c7be380d6739ddcba71
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-09 09:31:09 -0700

    1

--- WrestlerLogin.php
+++ WrestlerLogin.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 <?php 
-include "header.php";
 session_start();
 ?>
+<head>
+	<title>Borah High School Wrestling</title>
+	<link rel="stylesheet" href="Style.CSS">
+	<link rel="icon" href="Logo.jpg" type="image/gif" sizes="16x16">
+</head>
+<h1>
+	<img src="transparent.png" alt="Borah Wrestler" height="25%" width="25%">
+</h1>
+<div id="navigation">
+<ul class="nav">
+	<li class="nav-item"><a href ="CoachingStaff.php">Home</a></li>
+	<!-- <li class="nav-item"><a href ="Schedule.php">Schedule</a></li> -->
+	<li class="nav-item"><a href ="WrestlerLogin.php">Wrestler Login</a></li>
+	<!-- <li class="nav-item"><a href ="CoachingStaff.php">Coaching Staff</a></li> -->
+	<!-- <li class="nav-item"><a href ="LogoutHandler.php">Login Out</a></li> -->
+	<!-- <li class="nav-item"><a href ="forms.php">Wrestler's Forms</a></li> -->
+
+</ul>
+</div>
+
+
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 
 <body>
