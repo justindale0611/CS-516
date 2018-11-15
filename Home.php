90c7eba031c4a26aa676ab01695c81a2bb974e8b
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-14 20:25:23 -0700

    hw6

--- Home.php
+++ Home.php
@@ -29,7 +29,9 @@
 
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 <body>
-<p>Welcome to Borah High School's wrestling page. This page contains all information needed for wrestlers, parents, fans, and administrators. We cannot thank you enough for your support and look forward to seeing you at our next match.
+<p>
+<link href="https://fonts.googleapis.com/css?family=Contrail+One" rel="stylesheet">
+	Welcome to Borah High School's wrestling page. This page contains all information needed for wrestlers, parents, fans, and administrators. We cannot thank you enough for your support and look forward to seeing you at our next match.
 <img src="Alias.jpg" alt="Borah Wrestler" height="25%" width="25%" class="center">
 </p>
 </body>
