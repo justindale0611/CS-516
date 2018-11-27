99f372b5f6ff37997d68e4f3ef6bec2e20d257f2
Justin Gardner <Justin.Gardner@boiseschools.org>
2018-11-26 18:56:11 -0700

    hw6

--- Schedule.php
+++ Schedule.php
@@ -12,8 +12,9 @@ require_once("header.php");?>
 <link rel="stylesheet" type="text/css" href="Style.CSS">
 
 <body  id = schedule>
-<br><div id=schedulediv> LET'S GO LIONS! WIN WIN WIN!</div>
-<p><object height="400" width="800" class="center" data="Schedule.pdf"></object>
+<p>
+<div id=schedulediv> LET'S GO LIONS! WIN WIN WIN!</div>
+<object height="400" width="800" class="center" data="Schedule.pdf"></object>
 </p>
 </body>
 <?php 
